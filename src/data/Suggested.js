6dbd3cd414aeddd8747d46a35f3c4adcb2982184
unknown <mohammadbaghermollanory@gmail.com>
2024-09-10 02:17:58 +0330

    fake data

---
+++ src/data/Suggested.js
@@ -0,0 +1,24 @@
+const Suggested = [
+    {
+        id: 1,
+        title: "Travis Howard",
+        img: "/Images/1.jpg"
+
+    },{
+        id: 2,
+        title: "Remy Sharp",
+        img: "/Images/2.jpg"
+
+    },    {
+        id: 3,
+        title: "Cindy Baker",
+        img: "/Images/3.jpg"
+
+    },    {
+        id: 4,
+        title: "Agnes Walker",
+        img: "/Images/4.jpg"
+
+    }
+]
+export default Suggested
