59ec13c09b971fa169ccd6b25501e2e95ff64e87
unknown <mohammadbaghermollanory@gmail.com>
2024-09-10 02:17:29 +0330

    fake data

---
+++ src/data/Post.js
@@ -0,0 +1,37 @@
+const Post = [
+    {
+        id: 1,
+        title: 'fati',
+        city: 'shiraz',
+        img: "/Images/7.jpg",
+        postImg: '/Images/4.jpg',
+        caption1: " This impressive paella is a perfect party dish and a fun meal to cooktogether with your guests. Add 1 cup of frozen peas along with the mussels,if you like.",
+        caption2: " Heat 1/2 cup of the broth in a pot until simmering, add saffron and setaside for 10 minutes.",
+        caption3: " Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet overmedium-high heat. Add chicken, shrimp and chorizo, and cook, stirrioccasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to alarge plate and set aside, leaving chicken and chorizo in the pan. Addpimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook",
+
+        caption4: " Add rice and stir very gently to distribute. Top with artichokes and peppers",
+    },    {
+        id: 2,
+        title: 'moein',
+        city: 'tabriz',
+        img: "/Images/2.jpg",
+        postImg: '/Images/3.jpg',
+        caption1: " This impressive paella is a perfect party dish and a fun meal to cooktogether with your guests. Add 1 cup of frozen peas along with the mussels,if you like.",
+        caption2: " Heat 1/2 cup of the broth in a pot until simmering, add saffron and setaside for 10 minutes.",
+        caption3: " Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet overmedium-high heat. Add chicken, shrimp and chorizo, and cook, stirrioccasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to alarge plate and set aside, leaving chicken and chorizo in the pan. Addpimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook",
+
+        caption4: " Add rice and stir very gently to distribute. Top with artichokes and peppers",
+    },    {
+        id: 3,
+        title: 'zia',
+        city: 'esfahan',
+        img: "/Images/1.jpg",
+        postImg: './Images/8.jpg',
+        caption1: " This impressive paella is a perfect party dish and a fun meal to cooktogether with your guests. Add 1 cup of frozen peas along with the mussels,if you like.",
+        caption2: " Heat 1/2 cup of the broth in a pot until simmering, add saffron and setaside for 10 minutes.",
+        caption3: " Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet overmedium-high heat. Add chicken, shrimp and chorizo, and cook, stirrioccasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to alarge plate and set aside, leaving chicken and chorizo in the pan. Addpimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook",
+
+        caption4: " Add rice and stir very gently to distribute. Top with artichokes and peppers",
+    }
+]
+export default Post   
