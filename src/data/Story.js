a3124477572f1a792102c9ef840479991a022ac9
unknown <mohammadbaghermollanory@gmail.com>
2024-09-10 02:17:46 +0330

    fake data

---
+++ src/data/Story.js
@@ -0,0 +1,75 @@
+const Storys = [
+    {
+        id: 1,
+        title: "Travis Howard",
+        img: "/Images/1.jpg"
+
+    },{
+        id: 2,
+        title: "Remy Sharp",
+        img: "/Images/2.jpg"
+
+    },    {
+        id: 3,
+        title: "Cindy Baker",
+        img: "/Images/3.jpg"
+
+    },    {
+        id: 4,
+        title: "Agnes Walker",
+        img: "/Images/4.jpg"
+
+    },    {
+        id: 5,
+        title: "Henderson",
+        img: "/Images/5.jpg"
+
+    }, {
+        id: 6,
+        title: "Agnes Walker",
+        img: "/Images/6.jpg"
+
+    }, {
+        id: 7,
+        title: "Travis Howard",
+        img: "/Images/7.jpg"
+
+    }, {
+        id: 8,
+        title: "Cindy Baker",
+        img: "/Images/8.jpg"
+
+    }
+    , {
+        id: 9,
+        title: "Travis Howard",
+        img: "/Images/1.jpg"
+
+    },    {
+        id: 10,
+        title: "Remy Sharp",
+        img: "/Images/2.jpg"
+
+    },    {
+        id: 11,
+        title: "Cindy Baker",
+        img: "/Images/3.jpg"
+
+    },{
+        id: 12,
+        title: "Trevor Henderson",
+        img: "/Images/6.jpg"
+
+    }, {
+        id: 13,
+        title: "Trevor Henderson",
+        img: "/Images/7.jpg"
+
+    }, {
+        id: 14,
+        title: "Trevor Henderson",
+        img: "/Images/8.jpg"
+
+    }
+]
+export default Storys
