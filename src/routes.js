d515660566f4b28f0140e88203f82abfae39e597
unknown <mohammadbaghermollanory@gmail.com>
2024-09-19 09:01:28 +0330

    update to nested routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 import Profile from "./pages/profile/Profile"
 import Home from './components/home/Home'
+import { Children } from "react"
+import Posts from "./pages/posts/Posts"
+import Reels from "./pages/reels/Reels"
+import Tagged from "./pages/tagged/Tagged"
 
 
 let routes = [
-          { path: '/', element: <Home /> },
-          { path: '/home', element: <Home /> },
-          { path: '/profile', element: <Profile /> },
+  { path: '/', element: <Home /> },
+  { path: '/home', element: <Home /> },
+  {
+    path: '/profile/*', element: <Profile />, children: [
+      { path: 'posts', element: <Posts /> },
+      { path: 'reels', element: <Reels /> },
+      { path: 'tagged', element: <Tagged /> },
+    ]
+  },
+
 ]
 
 export default routes
