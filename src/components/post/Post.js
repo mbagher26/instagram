1259203331a1337ee0763ea7069aa99051495144
unknown <mohammadbaghermollanory@gmail.com>
2024-09-23 13:46:09 +0330

    conditional for display icon

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -2,21 +2,26 @@ import ReelsIcons from "../../Icons/ReelsIcon";
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import './Post.css';
 
+
+
 function Post({ BiglowteaPost, width, height }) {
+
+
+
     return (
         <>
 
-                {BiglowteaPost && BiglowteaPost.map(item => (
-                    <div className="post">
-                        <img style={{ width: width, height: height }} key={item.id} src={item.img} alt={item.title} />
-                        <div className="post-icon">
-                            {item.icon === 'shop' && <LocalMallIcon  />}
-                            {item.icon === '' && null}
-                            {item.icon === 'reels' && <ReelsIcons  width={18} height={18} />}
-                        </div>
+            {BiglowteaPost && BiglowteaPost.map(item => (
+                <div className="post">
+                    <img style={{ width: width, height: height }} key={item.id} src={item.img} alt={item.title} />
+                    <div className="post-icon">
+                        {item.icon === 'shop' && <LocalMallIcon />}
+                        {item.icon === '' && null}
+                        {item.icon === 'reels' && <ReelsIcons width={18} height={18} />}
                     </div>
-                ))}
-            
+                </div>
+            ))}
+           
         </>
     );
 }
