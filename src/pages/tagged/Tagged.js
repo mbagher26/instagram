26afb9b314aedd8ee02e54a7e0a506decb1941b8
unknown <mohammadbaghermollanory@gmail.com>
2024-09-25 09:54:02 +0330

    create Tagged component

---
+++ src/pages/tagged/Tagged.js
@@ -0,0 +1,11 @@
+function Tagged() {
+    return (
+        <div style={{backgroundColor: 'green'}}>
+            <h1>
+                TAGGED
+            </h1>
+        </div>
+    );
+}
+
+export default Tagged;
