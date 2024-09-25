ec80f8eafe9449da750986dfb6f15dfb7a0525fb
unknown <mohammadbaghermollanory@gmail.com>
2024-09-25 09:55:16 +0330

    create Reels component

---
+++ src/pages/reels/Reels.js
@@ -0,0 +1,11 @@
+function Reels() {
+    return (
+        <div style={{backgroundColor: 'green'}}>
+            <h1>
+                REELS
+            </h1>
+        </div>
+    );
+}
+
+export default Reels;
