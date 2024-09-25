544a2b0b684b2f6a036c8902370a7596f13daa4d
unknown <mohammadbaghermollanory@gmail.com>
2024-09-25 09:58:16 +0330

    add route for Shop component

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,20 @@ import { Children } from "react"
 import Posts from "./pages/posts/Posts"
 import Reels from "./pages/reels/Reels"
 import Tagged from "./pages/tagged/Tagged"
+import Shop from "./pages/shop/Shop"
 
 
 let routes = [
   { path: '/', element: <Home /> },
   { path: '/home', element: <Home /> },
+  { path: '/shop', element: <Shop /> },
+
   {
     path: '/profile/*', element: <Profile />, children: [
       { path: 'posts', element: <Posts /> },
       { path: 'reels', element: <Reels /> },
       { path: 'tagged', element: <Tagged /> },
+
     ]
   },
 
