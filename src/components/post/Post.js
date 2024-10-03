63a36f329e42a5603b5cfa8fb69c8fc0b1d92c42
unknown <mohammadbaghermollanory@gmail.com>
2024-10-03 09:36:54 +0330

    add new DialogContent

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -224,6 +224,36 @@ function Post({ BiglowteaPost, width, height }) {
                             </div>
                         </div>
                     </DialogContent>
+                    <DialogContent>
+                        <div className="dialog-content">
+                            <div className="content">
+                                <img src="/Images/cup.jpg" alt="cup" />
+                            </div>
+                            <div className="description">
+                                <div className="header">
+                                    <div className="header-bio">
+                                        <img alt="bigelow" src="/Images/bigelow.jpg" />
+                                        <div className="header-content">
+                                            <div className="header-title">
+                                                <a>bigelowtea</a>
+                                                <svg aria-label="Verified" className="x1lliihq x1n2onr6" fill="rgb(0, 149, 246)" height="18" role="img" viewBox="0 0 40 40" width="18"><title>Verified</title><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fillRule="evenodd"></path></svg>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <IconButton aria-label="more-header" >
+                                        <MoreHorizIcon className="icon-more-header" />
+                                    </IconButton>
+                                </div>
+
+                                <div className="post-show">
+                                  {BiglowteaPost && BiglowteaPost.map(item =>
+                                    <img style={{width:'208px', height:'200px', margin: '4px 0'}} key={item.id} src={item.img} alt={item.title}/>
+                                  )}
+                                </div>
+                            </div>
+                        </div>
+                    </DialogContent>
                 </Slider>
             </Dialog>
         </>
